efd9112fc96683827d1d9255477ef634eab9274c
JungCode <buidinhtrung0704@gmail.com>
2025-05-01 16:11:34 +0700

    feat(auth): SPA setting up #1

--- src/views/modules/routerConfig.js
+++ src/views/modules/routerConfig.js
@@ -1,5 +1,21 @@
 import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 
 export const initRoute = (route) => {
   route.addRoute('/login', Login());
+  route.addRoute('/sign-up', SignUp());
+
+}
+
+export const handleSPA = (route) => {
+  const content = document.querySelector('#content');
+  const allAnchor = document.querySelectorAll('a');
+  allAnchor.forEach((a) => {
+    a.addEventListener('click', (event)=>{
+      event.preventDefault();
+      content.classList.remove('fade-in');
+      history.pushState({}, '', a.href);
+      route.changeRoute();
+    });
+  });
 }
